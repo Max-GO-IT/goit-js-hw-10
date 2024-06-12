408d99b84a1e155103e68013842d56e7b2116355
Max-GO-IT <64358225+Max-GO-IT@users.noreply.github.com>
2024-06-12 21:05:15 +0000

    Deploying to gh-pages from @ Max-GO-IT/goit-js-hw-10@e974f4a19d3ffe8be4045270b94fe435b66615e0 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{i}from"./assets/vendor-77e16229.js";const o=document.querySelector(".form");o.addEventListener("submit",m);function m(e){e.preventDefault();const s=Number(e.target.elements.delay.value),r=e.target.elements.state.value;n(s,r).then(t=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${t}ms`})}).catch(t=>{i.error({title:"Error",message:`❌ Rejected promise in ${t}ms`})})}function n(e,s){return new Promise((r,t)=>{setTimeout(()=>{s==="fulfilled"?r(e):t(e)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
